0db42ae126a1acd040b34e225537e5b75869b3d4
RoureYungLoo <ruoyang8715@163.com>
2023-06-06 11:10:15 +0800

    静态库链接静态库

--- CMake/test2/sort.cpp
+++ CMake/test2/sort.cpp
@@ -1,8 +1,12 @@
 #include "sort.h"
 
 #include <iostream>
+
+#include "calc.h"
 int main(int argc, char const *argv[]) {
   insert();
   select();
+  double ret = my_add(3, 4);
+  std::cout << "sort测试中调用calc add: 3+4=" << ret << std::endl;
   return 0;
 }
