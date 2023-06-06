9cac98fca86bfdd0a3e2643ffc1548248593f3ea
RoureYungLoo <ruoyang8715@163.com>
2023-06-06 09:50:59 +0800

    宏定义

--- CMake/main.cpp
+++ CMake/main.cpp
@@ -4,7 +4,7 @@
  * @Author: luruoyang
  * @Date: 2023-06-05 17:13:25
  * @LastEditors: luruoyang
- * @LastEditTime: 2023-06-05 18:22:20
+ * @LastEditTime: 2023-06-06 09:46:33
  */
 
 #include <iostream>
@@ -17,6 +17,11 @@ int main(void) {
   b = my_sub(8, 2);
   c = my_multi(8, 2);
   d = my_div(8, 2);
+
+#ifdef DEBUG
+  printf("这是一行日志\n");
+#endif
+
   std::cout << "加：8+2=" << a << std::endl;
   std::cout << "减：8-2=" << b << std::endl;
   std::cout << "乘：8*2=" << c << std::endl;
