7fab1a5f83f8cdf60fd3da51760e46bdd13b6fda
RoureYungLoo <ruoyang8715@163.com>
2023-06-23 20:47:35 +0800

    commit

--- Lua/18-数据库访问.lua
+++ Lua/18-数据库访问.lua
@@ -1,1 +1,25 @@
 #!/usr/local/bin/lua
+
+luasql= require "luasql.mysql"
+
+env=luasql.mysql()
+
+conn=env:connect("test","root","lry216216","172.26.143.243",3309)
+
+conn:execte"SET NAMES UTF8"
+
+cur=conn:execte("select * form role")
+
+row=cur:fetch({},"a")
+
+file = io.open("role.txt","w+");
+while row do 
+    var =string.format("%d %s\n",row.id,row.name)
+    print(var)
+    file:write(var)
+    row=cur:fetch(row,"a")
+end
+
+file:close()
+conn:close()
+env:close()
