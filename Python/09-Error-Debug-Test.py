135434cda036d7d7002f1d0da8a593d5131ce194
RoureYungLoo <114283464+RoureYungLoo@users.noreply.github.com>
2023-08-29 14:04:11 +0800

    Update 09-Error-Debug-Test.py

--- Python/09-Error-Debug-Test.py
+++ Python/09-Error-Debug-Test.py
@@ -79,3 +79,96 @@ def bar2():
         raise
 
 bar2()
+# 调试
+# print
+
+# 断言 assert Ture，否则报错
+def foo3(s):
+    n=int(s)
+    assert n!=0,"n can not be zero!"
+    return 10/n
+# foo3(2)
+
+# logging
+import logging
+# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
+def foo4(s):
+    n=int(s)
+    logging.info("----------n=--------------%d"%n)
+    print(10/n)
+# foo4(0)
+
+# pdb
+
+# pdb.set_trace()
+import pdb
+
+def foo5(s):
+    n=int(s)
+    logging.info("----------n=--------------%d"%n)
+    # pdb.set_trace()
+    print(10/n)
+# foo5(0)
+
+#IDE [Visual Studio Code] [PyCharm]
+
+# logging 日志 ，关键日志输出
+
+#TDD Test-Driven Development 测试驱动开发
+import unittest
+from mydict import Dict
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+        
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+        
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+        
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+            
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+    
+    def setUp(self):
+        print("set up...")
+        
+    def tearDown(self):
+        print("tearDown...")
+            
+# if __name__=="__main__":
+#     unittest.main()
+    
+# 文档测试
+def abs(n):
+    '''    
+    Example:
+    
+    >>> abs(1)
+    1
+    >>> abs(-1)
+    1
+    >>> abs(0)
+    0
+    '''
+    return n if n>=0 else (-n)
+
+if __name__=="__main__":
+    import doctest
+    doctest.testmod()
