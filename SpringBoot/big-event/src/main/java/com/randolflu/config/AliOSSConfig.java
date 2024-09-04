505eeedb8a49c945741a03b266ab820ed09e45c2
RoureYungLoo <114283464+RoureYungLoo@users.noreply.github.com>
2024-09-04 21:42:16 +0800

    commit

--- SpringBoot/big-event/src/main/java/com/randolflu/config/AliOSSConfig.java
+++ SpringBoot/big-event/src/main/java/com/randolflu/config/AliOSSConfig.java
@@ -4,12 +4,12 @@ import com.randolflu.security.Keys;
 
 public class AliOSSConfig {
 
-    public static final String ENDPOINT = "";
-
-    public static final String BUCKET_NAME = "";
-
-    public static final String ACCESS_KEY_ID = Keys.ACCESS_KEY_ID;
-
-    public static final String ACCESS_KEY_SECRET = Keys.ACCESS_KEY_SECRET;
+//    public static final String ENDPOINT = "";
+//
+//    public static final String BUCKET_NAME = "";
+//
+//    public static final String ACCESS_KEY_ID = Keys.ACCESS_KEY_ID;
+//
+//    public static final String ACCESS_KEY_SECRET = Keys.ACCESS_KEY_SECRET;
 
 }
